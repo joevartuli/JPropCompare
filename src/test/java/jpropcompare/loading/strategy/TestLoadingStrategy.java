720158d9ca383a82722ae145f967b278e4037d3f
Joseph Vartuli <joevartuli@gmail.com>
2011-10-13 22:09:20 +1100

    Added ignore file and added Loading Strategy Template

---
+++ src/test/java/jpropcompare/loading/strategy/TestLoadingStrategy.java
@@ -0,0 +1,22 @@
+package jpropcompare.loading.strategy;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: Joe Vartuli
+ * Date: 19/09/11
+ * Time: 4:00 PM
+ */
+public class TestLoadingStrategy {
+
+
+
+
+}
