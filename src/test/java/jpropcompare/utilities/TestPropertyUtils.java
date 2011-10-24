a8edc7e595c9834c0b5eed1a97b55114e7e22aa7
Joseph Vartuli <joevartuli@gmail.com>
2011-10-24 20:42:01 +1100

    Additional unit tests

--- src/test/java/jpropcompare/utilities/TestPropertyUtils.java
+++ src/test/java/jpropcompare/utilities/TestPropertyUtils.java
@@ -1,13 +1,40 @@
 package jpropcompare.utilities;
 
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 /**
  * User: Joe Vartuli
  * Date: 15/10/11
  */
 public class TestPropertyUtils {
 
-    public void testSsConceptuallyEqual() {
+    @Test
+    public void testCombinationOfConceptuallyEqualArrays() {
 
+        List<String> listOne = Arrays.asList("one", "two", "three", "four");
+        List<String> listTwo = Arrays.asList("one", "two", "three", "four");
+        List<String> listThree = Arrays.asList("four", "two", "three", "one");
+
+        assertTrue(PropertyUtils.isConceptuallyEqual(listOne, listTwo));
+        assertTrue(PropertyUtils.isConceptuallyEqual(listOne, listThree));
+    }
+
+    @Test
+    public void testCombinationOfInEqualArrays() {
+
+        List<String> listOne = Arrays.asList("one", "two", "three", "four");
+        List<String> listTwo = Arrays.asList("one", "two", "three");
+        List<String> listThree = Arrays.asList("one", "one", "two", "three", "four");
+        List<String> listFour = Arrays.asList("one", "two", "three", "four1");
+
+        assertFalse(PropertyUtils.isConceptuallyEqual(listOne, listTwo));
+        assertFalse(PropertyUtils.isConceptuallyEqual(listOne, listThree));
+        assertFalse(PropertyUtils.isConceptuallyEqual(listOne, listFour));
     }
 }
